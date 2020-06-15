2041fa2f64e50aee5d3a598a29d731939c4e8fca
Nikkydee <43099966+Nikkydee@users.noreply.github.com>
2020-06-15 21:57:23 +0100

    This is my second Assignment

--- src/Assignments/printNumber.java
+++ src/Assignments/printNumber.java
@@ -1,3 +1,5 @@
+package Assignments;
+
 public class printNumber {
 
     public static void main(String[] args){
--- src/Assignments/sumnaturalNumber.java
+++ src/Assignments/sumnaturalNumber.java
@@ -1,3 +1,5 @@
+package Assignments;
+
 public class sumnaturalNumber {
 
     public static class addNumber {

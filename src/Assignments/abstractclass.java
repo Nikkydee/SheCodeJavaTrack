7ea8b22afd426c876d461c4c4b737ba1b5a6c4ed
Nikkydee <43099966+Nikkydee@users.noreply.github.com>
2020-06-22 23:40:25 +0100

    Assignment2

---
+++ src/Assignments/abstractclass.java
@@ -0,0 +1,50 @@
+package Assignments;
+
+import java.awt.*;
+
+public class abstractclass {
+    final int a = 20;
+
+    public void show ( ) {
+        System.out.println ( "This method is to display" );
+
+
+    }
+    abstract public void calculateArea();
+    }
+
+
+public class Rectangle extends abstractclass {
+    public static void main (String args[]) {
+        Rectangle obj = new Rectangle ( );
+        obj.show ( );
+        //obj.b=100;
+    }
+    //I have not really gotten the abstract class well. I have to read OOP all over
+}
+
+//Interface is an abstract class used to group related methods with empty bodies
+/*1. An abstract class can have both abstract and non-abstract methods while The interface can have only abstract methods.
+   2.   Abstract class  does not support multiple inheritances while interface  supports multiple inheritances.
+     3. Abstract class  can provide the implementation of the interface whereasIt can not provide the implementation of the abstract class.
+      5.   An abstract class can have protected and abstract public methods in other words an interface can have only have public abstract methods.
+
+       6.  An abstract class can have final, static, or static final variable with any access specifier whereasThe interface can only have a public static final variable.*/
+
+
+/*abstract class Shape{
+   final int b = 20;
+   public void display(){
+     System.out.println("This is display method");
+   }
+   abstract public void calculateArea();
+}
+
+public class Rectangle extends Shape{
+   public static void main(String args[]){
+      Rectangle obj = new Rectangle();
+      obj.display();
+     //obj.b=200;
+  }
+  //public void calculateArea(){}
+}*/
